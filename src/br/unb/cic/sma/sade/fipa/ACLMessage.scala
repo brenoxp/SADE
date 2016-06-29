409ecf610bdc766817e2c28d873a8e9a1b12be9f
gvanerven <gvanerven@gmail.com>
2016-06-29 12:43:59 -0300

    change ACLMessage to return a ActorReference

--- src/br/unb/cic/sma/sade/fipa/ACLMessage.scala
+++ src/br/unb/cic/sma/sade/fipa/ACLMessage.scala
@@ -1,26 +1,27 @@
 package br.unb.cic.sma.sade.fipa
 
-import scala.collection.immutable.HashMap
+import scala.collection.immutable.Map
+import akka.actor.ActorRef
 
-case class ACLMessage(parameters: HashMap[ACLMessageParameter.ACLMessageParameter, Any]){
+case class ACLMessage(parameters: Map[ACLMessageParameter.ACLMessageParameter, Any]){
   def frame = "fipa-acl-message"
   def ontology = "fipa-acl"
+  def sender = parameters.get(ACLMessageParameter.SENDER).orNull.asInstanceOf[ActorRef]
+  def receiver = parameters.get(ACLMessageParameter.RECEIVER).orNull.asInstanceOf[ActorRef]
+  def reply_to = if(parameters.contains(ACLMessageParameter.REPLY_TO)){ 
+              parameters.get(ACLMessageParameter.REPLY_TO).orNull.asInstanceOf[ActorRef]
+            } else { parameters.get(ACLMessageParameter.SENDER).orNull.asInstanceOf[ActorRef] }
+  def content = parameters.get(ACLMessageParameter.CONTENT).orNull
   
-  def reply(performative: Performative.Performative, content: Any): ACLMessage = {
-    val to = if(parameters.contains(ACLMessageParameter.REPLY_TO)){ 
-              parameters.get(ACLMessageParameter.REPLY_TO) 
-            } else { parameters.get(ACLMessageParameter.SENDER) }
-    val sender = (ACLMessageParameter.SENDER -> parameters.get(ACLMessageParameter.RECEIVER))
-    val replyTo = (ACLMessageParameter.REPLY_TO -> parameters.get(ACLMessageParameter.RECEIVER))
-    val receiver = (ACLMessageParameter.RECEIVER -> to)                    
-    val perform = (ACLMessageParameter.PERFORMATIVE, performative)
-    val cont = (ACLMessageParameter.CONTENT, content)
-    val par = parameters - ACLMessageParameter.PERFORMATIVE - 
+  def reply(performative: Performative.Performative, contentTo: Any): ACLMessage = {
+    val par = (parameters - ACLMessageParameter.PERFORMATIVE - 
                  ACLMessageParameter.CONTENT - ACLMessageParameter.SENDER - 
-                 ACLMessageParameter.RECEIVER - ACLMessageParameter.REPLY_TO + 
-                 perform + cont + sender + replyTo + receiver
+                 ACLMessageParameter.RECEIVER - ACLMessageParameter.REPLY_TO) + 
+                 (ACLMessageParameter.SENDER -> parameters.get(ACLMessageParameter.RECEIVER).get) +
+                 (ACLMessageParameter.REPLY_TO -> parameters.get(ACLMessageParameter.RECEIVER).get) +
+                 (ACLMessageParameter.RECEIVER -> reply_to) +
+                 (ACLMessageParameter.PERFORMATIVE -> performative) +
+                 (ACLMessageParameter.CONTENT -> contentTo)
     ACLMessage(par)
   }
 }
-
-case class ACLWrapper(message: ACLMessage)
